70e03363f662847d418c371429f6ff95b340953e
islam-2003 <118468579+islam-2003@users.noreply.github.com>
2026-04-07 15:57:26 +0200

    UnionFind methods implemented

--- src/UnionFind.java
+++ src/UnionFind.java
@@ -15,13 +15,47 @@ public class UnionFind {
         communityCount = 0;
     }
 
-    public void add(String element) {
+    public void add(String author) {
 
-        if(!parent.containsKey(element)){
-            parent.put(element, element);
-            rank.put(element, 0);
-            size.put(element, 1);
+        if(!parent.containsKey(author)){
+            parent.put(author, author);
+            rank.put(author, 0);
+            size.put(author, 1);
             communityCount++;
         }
+    }
+
+    public String find(String author){
+        
+        String root = author;
+        while (!root.equals(parent.get(root))) {
+            root = parent.get(root);
+        }
+        while (!author.equals(root)){
+            
+            String newAuthor = parent.get(author);
+            parent.put(author, root);
+            author = newAuthor;
+        }
+        return root;
+    }
+
+    public void union(String author1, String author2){
+
+        String root1 = find(author1);
+        String root2 = find(author2);
+        if (root1.equals(root2)) return;
+        if (rank.get(root1) < rank.get(root2)) {
+            parent.put(root1, root2);
+            size.put(root2, size.get(root2) + size.get(root1));
+        } else if (rank.get(root1) > rank.get(root2)) {
+            parent.put(root2, root1);
+            size.put(root1, size.get(root1) + size.get(root2));
+        } else {
+            parent.put(root2, root1);
+            rank.put(root1, rank.get(root1) + 1);
+            size.put(root1, size.get(root1) + size.get(root2));
+        }
+        communityCount--;
     }
 }
