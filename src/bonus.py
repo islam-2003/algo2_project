ea056bac1476f646e4da5332b52b570daf0a4832
islam-2003 <118468579+islam-2003@users.noreply.github.com>
2026-04-26 05:50:10 +0200

    Refactor bonus.py

--- src/bonus.py
+++ src/bonus.py
@@ -18,12 +18,9 @@ def parse_communities(filename):
 
 def get_country(author):
     author_clean = re.sub(r'\s+\d+$', '', author).strip()
-    url = "https://api.openalex.org/authors?search=" + author_clean + "&per_page=1&mailto=islam.mekhiouba@ulb.be"
+    url = "https://api.openalex.org/authors?search=" + author_clean + "&per_page=1&"
     try:
-        headers = {
-            "User-Agent": "mailto:islam.mekhiouba@ulb.be"
-        }
-        response = requests.get(url, headers=headers)
+        response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
             print(data)
@@ -52,29 +49,28 @@ def get_countries_per_community(communities):
             counter[country] += 1
             # """D'après Claude, il faut faire une pause de 0.5s entre les requêtes
             #         pour éviter de surcharger l'API"""
-            time.sleep(3)
+            time.sleep(0.5)
         results.append(counter)
     return results
 
 def plot_countries(countries_per_community):
-    # 1. Collecter tous les pays uniques
+    #  Collecter tous les pays présents dans les communautés
     all_countries = set()
     for counter in countries_per_community:
         all_countries.update(counter.keys())
     all_countries = sorted(all_countries)
     
-    # 2. Pour chaque communauté, calculer la proportion par pays
-    # (nombre d'auteurs de ce pays / total auteurs de la communauté)
+
     propotions = []
     for counter in countries_per_community:
         total = sum(counter.values())
         proportions = [counter[country] / total for country in all_countries]
         propotions.append(proportions)
-    # 3. Tracer les barres empilées
+
     ind = np.arange(len(countries_per_community))  # position des communautés sur l'axe x
     bottom = np.zeros(len(countries_per_community))  # pour empiler les barres
     plt.figure(figsize=(12, 6))
-    # 4. Sauvegarder en PNG
+    # Sauvegarder en PNG
     for i, country in enumerate(all_countries):
         values = [prop[i] for prop in propotions]
         plt.bar(ind, values, bottom=bottom, label=country)
@@ -87,9 +83,8 @@ def plot_countries(countries_per_community):
     plt.savefig('../output/proportion_auteurs_par_pays.png')
 
 if __name__ == "__main__":
-    print(get_country("Dacheng Tao"))
-    # communities = parse_communities("../output/resultats_tache2_complet.txt")
-    # print(f"{len(communities)} communautés trouvées")
-    # countries = get_countries_per_community(communities)
-    # plot_countries(countries)
-    # print("Graphique sauvegardé sur le dossier output")
+    communities = parse_communities("../output/resultats_tache2_complet.txt")
+    print(f"{len(communities)} communautés trouvées")
+    countries = get_countries_per_community(communities)
+    plot_countries(countries)
+    print("Graphique sauvegardé sur le dossier output")
